4dfedf723d10d04ff052c588445ed8693ec689b0
Shariif-Rajpoot <mr8781256@gmail.com>
2025-01-18 17:19:57 +0500

    Code to display data

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <title>Book List</title>
+</head>
+<body>
+    <div class="container">
+        <header class="d-flex justify-content-between my-4">
+            <h1>Book List</h1>
+            <div>
+                <a href="create.php" class="btn btn-primary">Add New Book</a>
+            </div>
+        </header>
+        <table class="table table-bordered">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Title</th>
+                    <th>Author</th>
+                    <th>Type</th>
+                    <th>Description</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+        </table>
+    </div>
+</body>
+</html>
