f762a1cb8b165654e94477f5bb0dfde72a5ced8b
Shariif-Rajpoot <mr8781256@gmail.com>
2025-01-19 14:43:39 +0500

    Home Page

--- index.php
+++ index.php
@@ -14,6 +14,53 @@
                 <a href="create.php" class="btn btn-primary">Add New Book</a>
             </div>
         </header>
+        
+        <?php 
+            session_start();
+            if(isset($_SESSION['create']))
+            {
+                ?>
+                    <div class="alert alert-success">
+                        <?php 
+                            echo $_SESSION['create'];
+                            // to don't see the message again and again;
+                            unset($_SESSION['create']);
+                        ?>
+                    </div>
+                <?php
+            }
+        ?>
+        
+        <?php 
+            if(isset($_SESSION['update']))
+            {
+                ?>
+                    <div class="alert alert-success">
+                        <?php 
+                            echo $_SESSION['update'];
+                            // to don't see the message again and again;
+                            unset($_SESSION['update']);
+                        ?>
+                    </div>
+                <?php
+            }
+        ?>
+
+<?php 
+            if(isset($_SESSION['delete']))
+            {
+                ?>
+                    <div class="alert alert-success">
+                        <?php 
+                            echo $_SESSION['delete'];
+                            // to don't see the message again and again;
+                            unset($_SESSION['delete']);
+                        ?>
+                    </div>
+                <?php
+            }
+        ?>
+
         <table class="table table-bordered">
             <thead>
                 <tr>
@@ -21,10 +68,41 @@
                     <th>Title</th>
                     <th>Author</th>
                     <th>Type</th>
-                    <th>Description</th>
                     <th>Action</th>
                 </tr>
             </thead>
+            <tbody>
+                <?php 
+                    // including the path to the database connection file to form the connection.
+                    include("connect.php");
+                    // preparing query to fetch all records from books table.
+                    $sql = "select * from books";
+                    // result will be an object
+                    $result = mysqli_query($conn,$sql);
+                    // to see the result 
+                    // print_r($result);
+                    // convert result to an array
+                    while($row = mysqli_fetch_array($result))
+                    {
+                        // print_r($row);
+                        // php script ends 
+                        ?>
+                        <tr>
+                            <td><?php echo $row['id'];?></td>
+                            <td><?php echo $row['title'];?> </td>
+                            <td><?php echo $row['author'];?></td>
+                            <td><?php echo $row['type']; ?></td>
+                            <td>
+                                <a href="view.php?id=<?php echo $row['id']; ?>" class="btn btn-info">Read More</a>
+                                <a href="edit.php?id=<?php echo $row['id'];?>" class="btn btn-warning">Edit</a>
+                                <a href="delete.php?id=<?php echo $row['id'];?>" class="btn btn-danger">Delete</a>
+                            </td>
+                        </tr>
+                        <!-- php script starts -->
+                         <?php
+                    }
+                        ?>
+            </tbody>
         </table>
     </div>
 </body>
